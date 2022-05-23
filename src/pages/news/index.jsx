111595f0b62fba994d8ac28da57fbfe6ae0c87c7
inbo29 <enkhbold4885@gmail.com>
2022-05-23 16:09:48 +0800

    add news page

---
+++ src/pages/news/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import blog2Data from "../../data/blog2.json";
+import DarkTheme from "../../layouts/Dark";
+import NavbarFullMenu from "../../components/Navbar-full-menu/navbar.full-menu";
+import BlogList from "../../components/Blog-list/blog-list";
+import PageHeader from "../../components/Page-header/page-header";
+import Footer from "../../components/Footer/footer";
+
+const BlogGridDark = () => {
+	const navbarRef = React.useRef(null);
+	const logoRef = React.useRef(null);
+
+	
+	return (
+		<DarkTheme>
+		<div className="circle-bg">
+			<div className="circle-color fixed">
+			<div className="gradient-circle"></div>
+			<div className="gradient-circle two"></div>
+			</div>
+		</div>
+		<NavbarFullMenu nr={navbarRef} lr={logoRef} />
+		<PageHeader
+			title="News"
+		/>
+		<BlogList blogs={blog2Data} />
+        <Footer hideBGCOLOR />
+		</DarkTheme>
+	);
+};
+
+export default BlogGridDark;
