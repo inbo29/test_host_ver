e390c0b4260744c16f050a5e5ec023da86847109
inbo29 <enkhbold4885@gmail.com>
2022-05-23 15:25:43 +0800

    add about index page

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import blog3Data from "../../data/blog3.json";
+import NavbarFullMenu from "../../components/Navbar-full-menu/navbar.full-menu";
+import PagesHeader from "../../components/Pages-header/index";
+import DarkTheme from "../../layouts/Dark";
+import Team2 from "../../components/Team2/team2";
+import Footer from "../../components/Footer/footer";
+
+
+const About = () => {
+	const navbarRef = React.useRef(null);
+	const logoRef = React.useRef(null);
+
+	return (
+		<DarkTheme>
+		<NavbarFullMenu nr={navbarRef} lr={logoRef} />
+		<PagesHeader />
+		<Team2 />
+			{/* <CusAboutBlogGrid blogs={blog3Data} /> */}
+		{/* <CalendarGfg /> */}
+		<Footer hideBGCOLOR />
+		</DarkTheme>
+	);
+};
+
+export default About;
+
+
+
+
