820c065b219d17ceb363797141105081f636d9f9
inbo29 <enkhbold4885@gmail.com>
2022-05-24 14:22:50 +0800

    edit some code pro

--- src/components/Works-style2/works-style-uranus.jsx
+++ src/components/Works-style2/works-style-uranus.jsx
@@ -24,28 +24,32 @@ const WorksStyleUranus = ({ grid, hideFilter, filterPosition }) => {
                   Artist
                 </h3>
               </div>
-              <div className="author">
-                <div className="author-img">
-                  <img src="/img/blog/2-1.jpg" alt="" />
-                </div>
-                <div className="info text-center my-2">
-                  <h6>
-                    <span>Uranus :</span> Singer
-                  </h6>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas pariatur perferendis maiores maxime a omnis reprehenderit cumque deserunt eaque, tempore dolores molestiae! Sequi, reiciendis nobis. Rerum quod debitis est, repellendus suscipit officia libero, accusantium sit tempore repellat aspernatur fugit laboriosam voluptatibus aut minus adipisci quaerat. Itaque deleniti quos dignissimos minima?
-                  </p>
-                  <div className="social" >
-                    <a href="https://m.facebook.com/Uranus.bbb?_rdr" target="_blank" className="mx-2">
-                      <i className="fab fa-facebook-f"></i>
-                    </a>
-                    <a href="https://www.instagram.com/uranusuya/?igshid=1aa14xv6wpy08" target="_blank" className="mx-2">
-                      <i className="fab fa-instagram"></i>
-                    </a>
-                    <a href="https://www.youtube.com/c/Uyanga/featured?app=desktop&disable_polymer=true&itct=CBoQ8JMBGAAiEwiS_uGC79DvAhXJkXwKHZYDAsk%3D" target="_blank" className="mx-2">
-                      <i className="fab fa-youtube"></i>
-                    </a>
-                    
+            </div>
+            <div className="col-lg-8 col-md-10">
+              <div className="sec-head text-center">
+                <div className="author">
+                  <div className="author-img">
+                    <img src="/img/blog/2-1.jpg" alt="" />
+                  </div>
+                  <div className="info text-center my-2">
+                    <h6>
+                      <span>Uranus :</span> Singer
+                    </h6>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas pariatur perferendis maiores maxime a omnis reprehenderit cumque deserunt eaque, tempore dolores molestiae! Sequi, reiciendis nobis. Rerum quod debitis est, repellendus suscipit officia libero, accusantium sit tempore repellat aspernatur fugit laboriosam voluptatibus aut minus adipisci quaerat. Itaque deleniti quos dignissimos minima?
+                    </p>
+                    <div className="social" >
+                      <a href="https://m.facebook.com/Uranus.bbb?_rdr" target="_blank" className="mx-2">
+                        <i className="fab fa-facebook-f"></i>
+                      </a>
+                      <a href="https://www.instagram.com/uranusuya/?igshid=1aa14xv6wpy08" target="_blank" className="mx-2">
+                        <i className="fab fa-instagram"></i>
+                      </a>
+                      <a href="https://www.youtube.com/c/Uyanga/featured?app=desktop&disable_polymer=true&itct=CBoQ8JMBGAAiEwiS_uGC79DvAhXJkXwKHZYDAsk%3D" target="_blank" className="mx-2">
+                        <i className="fab fa-youtube"></i>
+                      </a>
+                      
+                    </div>
                   </div>
                 </div>
               </div>
