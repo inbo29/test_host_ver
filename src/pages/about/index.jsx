3db9563dfc12cb39fba3fa0963778fb84f9fa823
inbo29 <enkhbold4885@gmail.com>
2022-05-30 17:01:44 +0800

    edit comp and rename fullcalendar

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -4,6 +4,7 @@ import NavbarFullMenu from "../../components/Navbar-full-menu/navbar.full-menu";
 import PagesHeader from "../../components/Pages-header/index";
 import DarkTheme from "../../layouts/Dark";
 import Team2 from "../../components/Team2/team2";
+import EventCalendarTest from "../../components/Event-calendar/event-calendar-test";
 import Footer from "../../components/Footer/footer";
 
 
@@ -16,6 +17,7 @@ const About = () => {
 		<NavbarFullMenu nr={navbarRef} lr={logoRef} />
 		<PagesHeader />
 		<Team2 />
+		<EventCalendarTest />
 			{/* <CusAboutBlogGrid blogs={blog3Data} /> */}
 		{/* <CalendarGfg /> */}
 		<Footer hideBGCOLOR />
