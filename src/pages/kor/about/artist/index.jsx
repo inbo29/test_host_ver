a19d83e2a57c02594527caeb889cbb503a29ee75
inbo29 <enkhbold4885@gmail.com>
2022-06-02 17:53:19 +0800

    error cache

--- src/pages/kor/about/artist/index.jsx
+++ src/pages/kor/about/artist/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavbarFullMenuKor from "../../../../components/Navbar-full-menu/navbar.full-menu-kor";
 import DarkTheme from "../../../../layouts/Dark";
-import Team2Kor2 from "../../../../components/Team2/team2-1Kor";
+import Team2Kor2 from "../../../../components/Team2/team2-1kor";
 import FootKor from "../../../../components/Footer/footkor";
 
 
