b3b79a91a06202bec64a7861623a0070fe597989
inbo29 <enkhbold4885@gmail.com>
2022-11-04 09:39:39 +0800

    before version2

--- src/common/loadingPace.js
+++ src/common/loadingPace.js
@@ -3,10 +3,12 @@ const loadingPace = () => {
   Pace.on("start", function () {
     document.querySelector("#preloader").classList.remove("isdone");
     document.querySelector(".loading").classList.remove("isdone");
+    document.querySelector(".imgLoad").classList.remove("isdone");
   });
   Pace.on("done", function () {
     document.querySelector("#preloader").classList.add("isdone");
     document.querySelector(".loading").classList.add("isdone");
+    document.querySelector(".imgLoad").classList.add("isdone");
   });
 
   if (document.querySelector("body").classList.contains("pace-done")) {
@@ -16,6 +18,7 @@ const loadingPace = () => {
 
   window.addEventListener("load", () => {
     document.querySelector("#preloader").classList.add("isdone");
+    document.querySelector(".imgLoad").classList.add("isdone");
     document.querySelector(".loading").classList.add("isdone");
     if (document.querySelector('.pace-running.pace-running')) {
       document.querySelector('.pace-running.pace-running').classList.remove('pace-running')
